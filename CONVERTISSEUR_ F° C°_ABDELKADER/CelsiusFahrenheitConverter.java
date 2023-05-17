647f4d4d56a7960a772f22e7e67806a5c8f5e819
Lotfi Abdelkadir RABAH <87047304+lotfi8rbh@users.noreply.github.com>
2023-05-17 12:18:39 +0100

    Delete CONVERTISSEUR_ F° C°_ABDELKADER directory

--- CONVERTISSEUR_ F° C°_ABDELKADER/CelsiusFahrenheitConverter.java
+++
@@ -1,7 +0,0 @@
-package com.example.convertisseur_temp_abdelkader;
-
-public class CelsiusFahrenheitConverter {
-    public static double convertCelsiusToFahrenheit(double celsius) {
-        return (celsius * 9 / 5) + 32;
-    }
-}
--- CONVERTISSEUR_ F° C°_ABDELKADER/FahrenheitCelsiusConverter.java
+++
@@ -1,7 +0,0 @@
-package com.example.convertisseur_temp_abdelkader;
-
-public class FahrenheitCelsiusConverter {
-    public static double convertFahrenheitToCelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5 / 9;
-    }
-}
