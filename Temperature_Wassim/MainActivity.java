6526ecbf1b0d8aafe1797583960b947ae5ee8ddb
CHIKHI Wassim <98323324+vvazzim@users.noreply.github.com>
2023-05-17 11:59:54 +0100

    Add files via upload

---
+++ Temperature_Wassim/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.convertisseur_temp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText celsiusInput = findViewById(R.id.celsiusInput);
+        Button convertButton = findViewById(R.id.convertButton);
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double celsius = Double.parseDouble(celsiusInput.getText().toString());
+                double fahrenheit = celsius * 9/5 + 32;
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("FAHRENHEIT", fahrenheit);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Temperature_Wassim/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.convertisseur_temp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        double fahrenheit = getIntent().getDoubleExtra("FAHRENHEIT", 0);
+        TextView resultText;
+        resultText = findViewById(R.id.resultText);
+        resultText.setText("Result: " + fahrenheit + " degrees Fahrenheit");
+    }
+}
+
