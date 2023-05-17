4265b511f8a620dda9ad9e5a2257dafaab68a1b1
islam yazid <102356921+YAZZYD@users.noreply.github.com>
2023-05-17 11:53:14 +0100

    Add files via upload

---
+++ IslamYazidBenyakoub_TP4/Contact.java
@@ -0,0 +1,24 @@
+package com.example.contacte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Contact {
+    public String nom;
+    public String prenom;
+    public String telephone;
+    public Contact(String NomC, String PrenomC, String TelephoneC) {
+        nom = NomC;
+        prenom = PrenomC;
+        telephone = TelephoneC;
+    }
+    public String getNom() {return nom;}
+    public void setNom() {this.nom=nom;}
+    public String getPrenom() {return prenom;}
+    public void setPrenom() {this.prenom=prenom;}
+    public String getTelephone() {return telephone;}
+    public void setTelephone() {this.telephone=telephone;}
+}
+
