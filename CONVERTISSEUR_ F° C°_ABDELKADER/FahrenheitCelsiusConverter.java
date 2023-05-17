1cbf333d0ecf60596a601ee9a4c79c316710e888
Lotfi Abdelkadir RABAH <87047304+lotfi8rbh@users.noreply.github.com>
2023-05-17 12:17:41 +0100

    Add files via upload

---
+++ CONVERTISSEUR_ F° C°_ABDELKADER/FahrenheitCelsiusConverter.java
@@ -0,0 +1,7 @@
+package com.example.convertisseur_temp_abdelkader;
+
+public class FahrenheitCelsiusConverter {
+    public static double convertFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
