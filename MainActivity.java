6f2947b3a9120982dd9993d04509b589a6df3792
Lilmaymo <100352108+Lilmaymo@users.noreply.github.com>
2023-05-21 00:32:18 +0100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,94 +1,100 @@
-package com.example.bluetooth;
+package com.example.tp_calculatrice;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button turnOnButton;
-    private Button turnOffButton;
-    private BluetoothAdapter bluetoothAdapter;
+    EditText num1View = new EditText(this);
+    EditText num2View = new EditText(this);
+
+    RadioGroup radioGroup = new RadioGroup(this);
+    RadioButton add = new RadioButton(this);
+    RadioButton sub = new RadioButton(this);
+    RadioButton mult = new RadioButton(this);
+    RadioButton div = new RadioButton(this);
+
+    TextView resultView = new TextView(this);
+
+    Button calcul = new Button(this);
+    boolean isKeyboardOpen = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        turnOnButton = findViewById(R.id.BtnTurnon);
-        turnOffButton = findViewById(R.id.BtnTurnon);
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        turnOnButton.setOnClickListener(new View.OnClickListener() {
+        num1View.findViewById(R.id.editTextNumber1);
+        num2View.findViewById(R.id.editTextNumber2);
+        radioGroup.findViewById(R.id.radioGroup);
+        add.findViewById(R.id.radioButtonAdd);
+        sub.findViewById(R.id.radioButtonSub);
+        mult.findViewById(R.id.radioButtonMul);
+        div.findViewById(R.id.radioButtonDiv);
+        resultView.findViewById(R.id.textViewResult);
+        calcul.findViewById(R.id.buttonCalculate);
+
+        calcul.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if (bluetoothAdapter == null) {
-                    // Device doesn't support Bluetooth
-                    Toast.makeText(MainActivity.this, "Bluetooth is not supported on this device", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (!bluetoothAdapter.isEnabled()) {
-                        // Bluetooth is currently disabled, so enable it
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                                // TODO: Consider calling
-                                //    ActivityCompat#requestPermissions
-                                // here to request the missing permissions, and then overriding
-                                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                //                                          int[] grantResults)
-                                // to handle the case where the user grants the permission. See the documentation
-                                // for ActivityCompat#requestPermissions for more details.
-                                return;
-                            }
-                            startActivity(enableBluetoothIntent);
+            public void onClick(View view) {
+                try {
+                    int x = Integer.parseInt(num1View.getText().toString());
+                    int y = Integer.parseInt(num2View.getText().toString());
+
+                    int result = 0;
+
+                    if (add.isChecked()) {
+                        result = x + y;
+                    } else if (sub.isChecked()) {
+                        result = x - y;
+                    } else if (mult.isChecked()) {
+                        result = x * y;
+                    } else if (div.isChecked()) {
+                        if (y == 0) {
+                            resultView.setText("La division par zéro est impossible");
+                            return;
                         } else {
-                            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                            startActivityForResult(enableBluetoothIntent, RESULT_OK);
+                            result = x / y;
                         }
                     } else {
-                        // Bluetooth is already enabled
-                        Toast.makeText(MainActivity.this, "Bluetooth is already enabled", Toast.LENGTH_SHORT).show();
+                        resultView.setText("Sélectionnez une opération");
+                        return;
                     }
+
+                    resultView.setText(String.format("result = %d", result));
+
+                    if (isKeyboardOpen) {
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(num2View.getWindowToken(), 0);
+                        isKeyboardOpen = false;
+                    }
+                }catch (NumberFormatException e){
+                    Toast.makeText(this, "Saisissez un nombre entier valide", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        turnOffButton.setOnClickListener(new View.OnClickListener() {
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        num2View.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onClick(View v) {
-                if (bluetoothAdapter == null) {
-                    // Device doesn't support Bluetooth
-                    Toast.makeText(MainActivity.this, "Bluetooth is not supported on this device", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (bluetoothAdapter.isEnabled()) {
-                        // Bluetooth is currently enabled, so disable it
-                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                            // TODO: Consider calling
-                            //    ActivityCompat#requestPermissions
-                            // here to request the missing permissions, and then overriding
-                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                            //                                          int[] grantResults)
-                            // to handle the case where the user grants the permission. See the documentation
-                            // for ActivityCompat#requestPermissions for more details.
-                            return;
-                        }
-                        bluetoothAdapter.disable();
-                        Toast.makeText(MainActivity.this, "Bluetooth disabled", Toast.LENGTH_SHORT).show();
-                    } else {
-                        // Bluetooth is already disabled
-                        Toast.makeText(MainActivity.this, "Bluetooth is already disabled", Toast.LENGTH_SHORT).show();
-                    }
-                }
+            public void onFocusChange(View v, boolean hasFocus) {
+                isKeyboardOpen = hasFocus;
             }
         });
     }
